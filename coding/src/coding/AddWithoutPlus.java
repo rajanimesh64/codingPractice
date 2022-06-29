cdebba08f5a8f48042163dc2749f30378b8e0b0a
animesh <animesh@kali>
2022-06-29 09:24:36 +0530

    Code to add number-29th june 2022

---
+++ coding/src/coding/AddWithoutPlus.java
@@ -0,0 +1,35 @@
+package coding;
+
+public class AddWithoutPlus {
+	
+
+	/*
+	 * @Author-Animesh Raj
+	 * Code to add two numbers without using plus,with the help of Binary operators
+	 */
+	
+	public static void  addwithoutplusnew(int a,int b) {
+		
+		int exAdd=a^b;
+		int carry=a&b;
+		int c,d;
+		while(carry!= 0) {
+			c=carry << 1;
+			d=exAdd;
+			exAdd=c^d;
+			carry=c&d;
+			
+		}
+		System.out.println(exAdd);
+	}
+	
+	
+public static void main(String[] args) {
+	addwithoutplusnew(-10,200);
+	
+	
+	
+}
+
+
+}
