f0413a51202ec650e3d6b126d36c6051d00e0b61
animesh <animesh@kali>
2022-08-03 07:50:41 +0530

    kackerrank graph

--- coding/src/coding/SelectionSort.java
+++ coding/src/coding/SelectionSort.java
@@ -1,7 +1,9 @@
 package coding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
  * The Selection sort algorithm is based on the idea of finding 
@@ -11,6 +13,48 @@ import java.util.List;
  */
 
 public class SelectionSort {
+	
+	  public static int makeAnagram(String a, String b) {
+		    // Write your code here
+		    int common=0;
+		   final int uncommon=0;
+		    Map<Character,Integer> aFrequency= new  HashMap<>();
+		    Map<Character,Integer> bFrequency= new  HashMap<>();
+		    int i=0;
+		    int j=0;
+		    while(i != a.length()){
+		    	if(aFrequency.containsKey(a.charAt(i))) {
+		    		char tmp=a.charAt(i);
+		    		aFrequency.put(tmp,aFrequency.get(tmp)+1);       
+		    	}else {
+		    		aFrequency.put(a.charAt(i),1);
+		    	}i++;
+		    } 
+		    while(j != b.length()){
+		    	if(bFrequency.containsKey(b.charAt(j))) {
+		    		char tmp=b.charAt(j);
+		    		bFrequency.put(tmp,bFrequency.get(tmp)+1);       
+		    	}else {
+		    		bFrequency.put(b.charAt(j),1);
+		    	}j++;
+		    } 
+		    aFrequency.forEach((key,val) -> {
+		    	if(bFrequency.containsKey(key)) {
+		    		int tmp=Math.abs(val-bFrequency.get(key));
+		    		uncommon+=tmp;
+		    	}
+		    });
+		    
+		    
+		    int minimumdelete=(a.length()-common)+(b.length()-common);
+		    
+		    
+		    return minimumdelete;
+		    }
+
+		
+	
+	
 	static void sSort(List<Integer> inp) {
 		int endPointer=inp.size()-1;
 		int startingPointer=0;
@@ -35,6 +79,7 @@ public class SelectionSort {
 	}
 	
 	public static void main(String[] args) {
+		makeAnagram("fcrxzwscanmligyxyvym","jxwtrhvujlmrpdoqbisbwhmgpmeoke");
 		List<Integer> input= new ArrayList<>();
 //		input.add(10);
 //		input.add(3);
@@ -46,7 +91,7 @@ public class SelectionSort {
 		input.add(4);
 		input.add(5);
 		input.add(6);
-		sSort(input);
+		//sSort(input);
 	}
 
 }
